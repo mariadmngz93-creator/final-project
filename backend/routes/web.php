fef880068e2f156928d7535978ebf1fb32dfa854
mariadmngz93-creator <mariadmngz93@gmail.com>
2025-10-16 01:49:07 +0800

    Revert "init backend setup"
    This reverts commit e19345508a7d00ecd9b317c2848386b48c0f9da1.

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,8 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth'])->name('dashboard');
-// require __DIR__.'/auth.php';
-Route::get('user/{hash}', UserController::class . '@show')->name('user.show');
