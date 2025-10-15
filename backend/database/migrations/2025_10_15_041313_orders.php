e19345508a7d00ecd9b317c2848386b48c0f9da1
mariadmngz93-creator <mariadmngz93@gmail.com>
2025-10-15 12:55:32 +0800

    init backend setup

--- backend/database/migrations/2025_10_15_041313_orders.php
+++ backend/database/migrations/2025_10_15_041313_orders.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
